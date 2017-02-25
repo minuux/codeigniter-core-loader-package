455d6c5dd414b63c28896766d03656a8690e42f1
minuux <61769459@qq.com>
2017-02-25 20:33:13 +0800

    add readme rename package

--- codeigniter_core_loader_package.php
+++ codeigniter_core_loader_package.php
@@ -2,43 +2,43 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-trait codeigniter_core_loader_packages {
+trait codeigniter_core_loader_package {
 
     public function package($package_name, $params, $object_name = NULL) {
         $packages = config_item('packages');
         $package = (object) $packages[$package_name];
         switch ($package->type) {
             case 'codeigniter-library':
-                return $this->_library($package, $params, $object_name);
+                return $this->_package_library($package, $params, $object_name);
             case 'codeigniter-view':
                 //视图中的object_name是用来设置是否返回view的
-                return $this->_view($package, $params, $object_name === NULL ? FALSE : TRUE);
+                return $this->_package_view($package, $params, $object_name === NULL ? FALSE : TRUE);
             case 'codeigniter-helper':
-                return $this->_helper($package);
+                return $this->_package_helper($package);
             case 'codeigniter-model':
-                return $this->_model($package, $object_name);
+                return $this->_package_model($package, $object_name);
         }
 
         return $this;
     }
 
-    private function _library($package, $params = NULL, $object_name = NULL) {
+    private function _package_library($package, $params = NULL, $object_name = NULL) {
         include_once($package->path);
         return $this->_ci_init_library($package->class_name, '', $params, $object_name);
     }
 
-    private function _view($package, $vars = array(), $return = FALSE) {
+    private function _package_view($package, $vars = array(), $return = FALSE) {
         return $this->_ci_load(array('_ci_path' => $package->path, '_ci_vars' => $vars, '_ci_return' => $return));
     }
 
-    private function _helper($package) {
+    private function _package_helper($package) {
         if (!isset($this->_ci_helpers[$package->name])) {
             include_once($package->path);
             $this->_ci_helpers[$package->name] = TRUE;
         }
     }
 
-    private function _model($package, $object_name = NULL) {
+    private function _package_model($package, $object_name = NULL) {
         if (in_array($package->name, $this->_ci_models, TRUE)) {
             return $this;
         }
